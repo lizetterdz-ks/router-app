1c4a17399fe9d3feee7aadeea7ef27bde4e71f74
lizetterdz <71050320+lizetterdz@users.noreply.github.com>
2021-11-18 13:03:38 -0600

    configured app routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import PostView from './views/PostView';
+import PostDashboard from './views/PostDashboard';
+import Home from './views/Home'
+import Login from './views/Login';
+import NotFoundScreen from './views/NotFoundScreen';
+import { ProtectedRoute } from './components/PrivateRoute';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' exact element={<Home />}/>
+        <Route path='/home' exact element={<Home />}/>
+        <Route path='/login' exact element={<Login />} />
+        <Route path='/posts' exact element={<PostDashboard />} />
+        <Route
+        path="/posts/:id"
+        element={
+          <ProtectedRoute redirectTo="/login">
+            <PostView />
+          </ProtectedRoute>
+        }
+        />
+        <Route path="*" element={<NotFoundScreen />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
